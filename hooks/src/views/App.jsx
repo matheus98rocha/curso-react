542c72306534602ce0ce3b7efcffbf95acb68843
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-01-06 18:45:41 -0300

    Use Context #1

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -6,7 +6,6 @@ import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
 const App = props => {
-
     return (
                 <div className="App">
                     <Router>

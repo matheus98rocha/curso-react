21d26574e2223fbbc8354778918d6585c77d54bb
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-01-07 20:29:53 -0300

    UseContext #2

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -4,11 +4,9 @@ import React from 'react'
 
 import App from './views/App'
 
-import DataContext, { data } from './data/DataContext';
 
 ReactDOM.render(
-    <DataContext.Provider value={data}>
-        <App />
-    </DataContext.Provider>,
+
+    <App />,
     document.getElementById('root')
 )

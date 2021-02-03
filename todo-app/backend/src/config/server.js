f1a93acd0d6913c7f6f3d73b75f686474dcdae80
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-02-02 23:02:40 -0300

    Configurando Backend

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -6,3 +6,7 @@ const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+
+server.listen(port,function(){
+    console.log(`BACKEND is runing on port ${port}.`)
+})

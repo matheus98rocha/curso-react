a702fab54fb89ac5eb68a0143aa5c30b1556ade5
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-02-02 23:38:49 -0300

    Finalizando o Backend

--- todo-app/backend/src/api/todo/todo.js
+++ todo-app/backend/src/api/todo/todo.js
@@ -4,7 +4,7 @@ const mongoose = restful.mongoose;
 const todoSchema = new mongoose.Schema({
     description: {type: String, require: true},
     done: {type: Boolean, require: true, default: false},
-    createdAt: {type: Data, default: Date.now}
+    createdAt: {type: Date, default: Date.now}
 })
 
 module.exports = restful.model('Todo', todoSchema);

61bfe3080e9c21afb0df26ab11c0103cea6c79e3
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-02-05 20:39:07 -0300

    Iniciando projeto react

---
+++ todo-app/fronted/src/main/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awersome.min.css';
+
+export default props => {
+return(<div className='container'>
+    <h1>Matheus Gonçalves Rocha</h1>
+</div>
+)}
